78590768490403d5924933793ae2c882781214ca
Prajwal Raj Basnet <basnetprajwalraj@gmail.com>
2021-05-18 16:46:34 +0545

    Implement accordion with basic styles

--- Accordion/main.js
+++ Accordion/main.js
@@ -1,9 +1,30 @@
 const template = document.createElement('template');
 template.innerHTML = `
-  <style></style>
+  <style>
+    *{
+      font-family: 'Open Sans', sans-serif;
+    }
+    #header{
+      display: flex;
+      justify-content: space-between;
+      padding: 15px 20px;
+      background-color: #edece8;
+      border: 1px solid #404040;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    .content{
+      margin-left: 20px;
+      display: none;
+    }
+    .content-active{
+      display: block;
+    }
+  </style>
   <div class="accordion">
-    <div class="header">
+    <div id="header">
       <slot name="header"></slot>
+      <i id="indicator" class="fas fa-chevron-right"></i>
     </div>
     <div class="content">
       <slot name="content"></slot>
@@ -14,9 +35,28 @@ template.innerHTML = `
 class AccordionItem extends HTMLElement {
 	constructor() {
 		super();
-		this.attachShadow({ mode: open }).appendChild(
+		this.attachShadow({ mode: 'open' }).appendChild(
 			template.content.cloneNode(true)
 		);
+	}
+
+	connectedCallback() {
+		this.setAttribute('data-open', false);
+
+		const header = this.shadowRoot.querySelector('#header');
+		const content = this.shadowRoot.querySelector('.content');
+		const indicator = this.shadowRoot.querySelector('#indicator');
+
+		header.addEventListener('click', () => {
+			content.classList.toggle('content-active');
+			if (this.getAttribute('data-open') === 'false') {
+				this.setAttribute('data-open', true);
+				indicator.classList = 'fas fa-chevron-down';
+			} else {
+				this.setAttribute('data-open', false);
+				indicator.classList = 'fas fa-chevron-right';
+			}
+		});
 	}
 }
 customElements.define('accordion-item', AccordionItem);
